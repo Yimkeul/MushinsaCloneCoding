704452fbcdad254d46124f8f57b6027558b9f323
Yimkeul <leesjun29@gmail.com>
2024-08-19 05:13:26 +0900

    feat : tabbarController 이미지, title 추가

---
+++ MushinsaClone/TabViewGroup/SnapView/SnapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SnapViewController.swift
+//  MushinsaClone
+//
+//  Created by yimkeul on 8/19/24.
+//
+
+import UIKit
+
+class SnapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
